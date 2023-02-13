50e25a888ad7eac4e74b91f678fe13578d982034
Amelie Eveno <amelie.eveno@insa-rennes.fr>
2023-02-14 00:28:17 +0100

    corners detection

--- OpenCV1/OpenCV1.cpp
+++ OpenCV1/OpenCV1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -13,6 +12,7 @@
 
 constexpr auto X_ZONING = 3;
 constexpr auto Y_ZONING = 3;
+int maxCorners = 10;
 
 using namespace cv;
 using namespace std;
@@ -77,6 +77,76 @@ vector<double> centerOfMassComputing(const Mat& inputImg) {
     return {m.m10 / m.m00, m.m01 / m.m00};
 }
 
+vector<Point> cornerHarrisDetection(const Mat& inputImg) {
+    //Parameters for the detector
+    int thresh = 200;
+    int blockSize = 2;
+    int apertureSize = 3;
+    double k = 0.04;
+    Mat dst = Mat::zeros( inputImg.size(), CV_32FC1 );
+
+    vector<Point> listCornersCoordinates;
+
+    cornerHarris( inputImg, dst, blockSize, apertureSize, k);
+    Mat dst_norm, dst_norm_scaled;
+    normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
+    convertScaleAbs( dst_norm, dst_norm_scaled );
+    for( int i = 0; i < dst_norm.rows ; i++ )
+    {
+        for( int j = 0; j < dst_norm.cols; j++ )
+        {
+            int valueToTest = (int) dst_norm.at<float>(i,j);
+            if( valueToTest > thresh )
+            {
+                circle( dst_norm_scaled, Point(j,i), 5,  Scalar(0), 2, 8, 0 );
+                listCornersCoordinates.push_back(Point(j, i));
+            }
+        }
+    }
+
+    namedWindow( "Corners detected" );
+    imshow( "Corners detected", dst_norm );
+//    waitKey(0);
+    return listCornersCoordinates;
+}
+
+vector<Point> goodFeaturesToTrack(const Mat& inputImg)
+{
+    vector<Point> corners;
+    double qualityLevel = 0.01;
+    double minDistance = 10;
+    int blockSize = 3, gradientSize = 3;
+    bool useHarrisDetector = false;
+    double k = 0.04;
+    RNG rng(12345);
+
+    Mat copy = inputImg.clone();
+    goodFeaturesToTrack( inputImg,
+                         corners,
+                         maxCorners,
+                         qualityLevel,
+                         minDistance,
+                         Mat(),
+                         blockSize,
+                         gradientSize,
+                         useHarrisDetector,
+                         k );
+    cout << "** Number of corners detected: " << corners.size() << endl;
+    int radius = 4;
+    for( size_t i = 0; i < corners.size(); i++ )
+    {
+        circle( copy, corners[i], radius, Scalar(rng.uniform(0,255), rng.uniform(0, 256), rng.uniform(0, 256)), FILLED );
+    }
+    namedWindow( "Corners detected" );
+    imshow( "Corners detected", copy );
+//    waitKey(0);
+    return corners;
+}
+
+bool cmpDistToUpperLeft(const Point& p1, const Point& p2){
+    return ((pow(p1.y,2)+pow(p1.x,2)) < (pow(p2.y,2)+pow(p2.x,2)));
+}
+
 int main()
 {
     /**
@@ -94,8 +164,7 @@ int main()
     String imgsPath = "../images/exemples";
     ofstream arffFile("results.arff");
 
-    // ARFF file header
-    //TODO: generate header portions from old_features_modules classes' function
+    //ARFF file header
     list<pair<String, String>> attrList;
     attrList.push_back({ String("symbol"), symbol_list});
     attrList.push_back({ String("height"), num});
@@ -115,6 +184,13 @@ int main()
             attrList.push_back({ String("center_of_mass_Y_zone_" + to_string(i) + "_" + to_string(j) ), num});
         }
     }
+
+    /*
+    for(int i = 0; i < maxCorners; i++) {
+        attrList.push_back({String("corner_x_"+ to_string(i)), num});
+        attrList.push_back({String("corner_y_"+ to_string(i)), num});
+    }
+    */
 
     arffFile << generateARFFHeader("results", attrList);
 
@@ -166,8 +242,6 @@ int main()
         if (rad == 0.0) continue;
 
         // Size normalization
-        //TODO: size normalization without deformation
-        //TODO: define size (not hard-coded)
         Size newSize = Size(128, 128);
         resize(croppedImage, croppedImage, newSize);
 
@@ -199,10 +273,29 @@ int main()
             }
         }
 
+        // Corners
+        /*
+        //vector<Point> listCornersCoordinates = cornerHarrisDetection(croppedImage);
+
+        vector<Point> listCornersCoordinates = goodFeaturesToTrack(croppedImage);
+        std::sort(listCornersCoordinates.begin(), listCornersCoordinates.end(), cmpDistToUpperLeft);
+        for(const Point& p : listCornersCoordinates){
+            attributes.push_back(to_string(p.x));
+            attributes.push_back(to_string(p.y));
+        }
+        if(listCornersCoordinates.size() < maxCorners){
+            int missingCorners = maxCorners - listCornersCoordinates.size();
+            for(int i = 0; i < missingCorners; i++){
+                attributes.push_back("?");
+                attributes.push_back("?");
+            }
+        }
+        */
+
         arffFile << generateARFFLine(attributes);
 
         arffGenerationStep++;
-        cout << to_string(arffGenerationStep) << " : " << fileName << endl;
+        cout << to_string(arffGenerationStep) << " " << fileName << endl;
     }
     arffFile.close();
 
--- todos.md
+++ todos.md
@@ -1,9 +1,18 @@
+## Extraction de caractéristiques
+
 [X] binarisation  
 [X] extraction rectangle englobant : récupération des dimensions  
 [X] normalisation imagette en carré de 128*128  
 [X] zoning puis utilisation caractéristiques sur chaque zone :  
 	[X] centre de masse  
 	[X] densité (mean gray value) / ou proportion de pixels noirs  
-[] SIFT 
+[X] coins 
 
+## Entraînement de classifiers
+Etapes :  
+kNN = bonne prise de température  
+si les caractéristiques sont bonnes le kNN va bien marcher (car pas très performant globalement)  
+-> MLP / arbre de décision (on peut dépasser les kNN si on les paramètre bien)  
+-> SVM (encore mieux mais plus compliqué à mettre en œuvre)  
+-> si possible, bagging / boosting
 
