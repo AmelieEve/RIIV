b2d6762c003f56b9b33cafbe4d709c19836ded6d
Antoine.Lucas1 <antoine.lucas1@insa-rennes.fr>
2023-02-14 17:50:58 +0100

    enfin

--- OpenCV1/OpenCV1.cpp
+++ OpenCV1/OpenCV1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
--- OpenCV1/contours.cpp
+++ OpenCV1/contours.cpp
@@ -1,5 +1,25 @@
 #include "contours.h"
 
+Point center;
+
+struct contour_greater
+{
+    bool operator() (const Point& a, const Point& b) {
+        double angle1 = atan2(a.y - center.y, a.x - center.x) * 180.0 / CV_PI;
+        double angle2 = atan2(b.y - center.y, b.x - center.x) * 180.0 / CV_PI;
+        return (angle1 < angle2);
+    }
+};
+
+template<typename T>
+vector<T> flatten(const vector<vector<T>>& vec) {
+    vector<T> result;
+    for (auto const& v : vec) {
+        result.insert(result.end(), v.begin(), v.end());
+    }
+    return result;
+}
+
 void cropFromContour(const Mat& inputGrayImg, Mat& outputImg)
 {
     Mat workingImage = inputGrayImg.clone();
@@ -8,15 +28,10 @@ void cropFromContour(const Mat& inputGrayImg, Mat& outputImg)
     Canny(workingImage, workingImage, 0, 230);
     findContours(workingImage, contours, RETR_LIST, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    vector<Point> outerContour;
-    double maxContourArea = 0.0;
-    for (vector<Point> cont : contours) {
-        if (maxContourArea < contourArea(cont)) {
-            outerContour = cont;
-            maxContourArea = contourArea(cont);
-        }
-    }
-    Rect rectangle = boundingRect(outerContour);
+    vector<Point> allcontours = flatten(contours);
+    sort(allcontours.begin(), allcontours.end(), contour_greater());
+
+    Rect rectangle = boundingRect(allcontours);
 
     outputImg = inputGrayImg(rectangle);
 }
@@ -29,16 +44,10 @@ pair<double, double> contourAreaAndPerimeter(const Mat& inputGrayImg)
     Canny(workingImage, workingImage, 0, 230);
     findContours(workingImage, contours, RETR_LIST, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    vector<Point> outerContour;
-    double maxContourArea = 0.0;
-    for (vector<Point> cont : contours) {
-        if (maxContourArea < contourArea(cont)) {
-            outerContour = cont;
-            maxContourArea = contourArea(cont);
-        }
-    }
+    vector<Point> allcontours = flatten(contours);
+    sort(allcontours.begin(), allcontours.end(), contour_greater());
 
-    return { contourArea(outerContour), arcLength(outerContour, true) };
+    return { contourArea(allcontours), arcLength(allcontours, true) };
 }
 
 double equivalentRadius(const Mat& inputGrayImg)
@@ -49,16 +58,10 @@ double equivalentRadius(const Mat& inputGrayImg)
     Canny(workingImage, workingImage, 0, 230);
     findContours(workingImage, contours, RETR_LIST, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    vector<Point> outerContour;
-    double maxContourArea = 0.0;
-    for (vector<Point> cont : contours) {
-        if (maxContourArea < contourArea(cont)) {
-            outerContour = cont;
-            maxContourArea = contourArea(cont);
-        }
-    }
+    vector<Point> allcontours = flatten(contours);
+    sort(allcontours.begin(), allcontours.end(), contour_greater());
 
 
-    if (outerContour.size() < 1) return 0.0;
-    return sqrt(contourArea(outerContour) / acos(-1));
+    if (allcontours.size() < 1) return 0.0;
+    return sqrt(contourArea(allcontours) / CV_PI);
 }
